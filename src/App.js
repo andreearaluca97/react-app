cae29599f55640ec50e4bb9d8aaa177d50d359c0
Andreea <>
2021-07-15 14:20:54 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather React App
-      <footer>
-        Coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/andreea-raluca-lazareanu/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Andreea Raluca Lazareanu
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/andreearaluca97/react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather React App</h1>
+        <button className="btn btn-primary">Click Me</button>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/andreea-raluca-lazareanu/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Andreea Raluca Lazareanu
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/andreearaluca97/react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
