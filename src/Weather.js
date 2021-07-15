cf23861a7da432dbb947cb548180937c854a69ba
Andreea <>
2021-07-15 18:41:12 +0200

    Styled the component

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,20 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form className="search-engine">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter your city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
       <h1>Madrid</h1>
       <ul>
         <li>Wednesday 08: 00</li>
