2e37bc6f8fb6978f9fd35f8b03876be526f94b2a
Andreea <>
2021-07-15 14:06:46 +0200

    Added the Github Repo Url and LinkedIn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather React App</div>;
+  return (
+    <div className="App">
+      Weather React App
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/andreea-raluca-lazareanu/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Andreea Raluca Lazareanu
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/andreearaluca97/react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
