79b572dc5afc3c77d7b98c2d1decf659b31aaf5d
Andreea <>
2021-07-15 19:04:47 +0200

    Styling

--- src/Weather.js
+++ src/Weather.js
@@ -8,9 +8,9 @@ export default function Weather() {
         <div className="row">
           <div className="col-9">
             <input
+              className="form-control"
               type="search"
               placeholder="Enter your city"
-              className="form-control"
             />
           </div>
           <div className="col-3">
@@ -19,13 +19,14 @@ export default function Weather() {
         </div>
       </form>
       <h1>Madrid</h1>
-      <ul>
+      <ul className="city-description">
         <li>Wednesday 08: 00</li>
         <li>Cloudy</li>
       </ul>
       <div className="row">
         <div className="col-6">
           <img
+            className="image-weather"
             src="https://image.flaticon.com/icons/png/512/2204/2204339.png"
             width="70"
             height="70"
@@ -34,7 +35,7 @@ export default function Weather() {
           4º C
         </div>
         <div className="col-6">
-          <ul>
+          <ul className="weather-description">
             <li>Precipitation: 55%</li>
             <li>Humidity: 75%</li>
             <li>Wind: 20km/h</li>
