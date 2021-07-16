86d69da4fc5af880706d158de9b77cc218cb9832
Andreea <>
2021-07-16 18:59:16 +0200

    Fetching data from the Api URL

--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="search-engine">
-        <div className="row mt-3">
-          <div className="col-9">
-            <input
-              className="form-control"
-              type="search"
-              placeholder="Enter your city"
-              autoFocus="on"
-            />
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "https://image.flaticon.com/icons/png/512/2204/2204335.png",
+      date: "sunday 18:00",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="search-engine">
+          <div className="row mt-3">
+            <div className="col-9">
+              <input
+                className="form-control"
+                type="search"
+                placeholder="Enter your city"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn w-90" />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="search" className="btn w-90" />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul className="city-description">
+          <li className="text-capitalize">
+            <strong>{weatherData.date}</strong>
+          </li>
+          <li className="text-capitalize">
+            <strong>{weatherData.description}</strong>
+          </li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src={weatherData.iconUrl}
+              width="70"
+              height="70"
+              alt={weatherData.description}
+            ></img>
           </div>
-        </div>
-      </form>
-      <h1>Madrid</h1>
-      <ul className="city-description">
-        <li>
-          Wednesday <strong>08: 00</strong>
-        </li>
-        <li>
-          <strong>Cloudy</strong>
-        </li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://image.flaticon.com/icons/png/512/2204/2204339.png"
-            width="70"
-            height="70"
-            alt="cloudy"
-          ></img>
-        </div>
-        <div className="col-6">
-          <ul className="weather-description">
-            <li>
-              Temperature: <strong>4º C | F</strong>
-            </li>
-            <hr />
-            <li>
-              Precipitation: <strong>55%</strong>
-            </li>
-            <li>
-              Humidity: <strong>75%</strong>
-            </li>
-            <li>
-              Wind: <strong>20km/h</strong>
-            </li>
-          </ul>
+          <div className="col-6">
+            <ul className="weather-description">
+              <li className="temperature">
+                Temperature:{" "}
+                <strong>{Math.round(weatherData.temperature)}ºC| F</strong>
+              </li>
+              <hr />
+
+              <li>
+                Humidity: <strong>{weatherData.humidity} %</strong>
+              </li>
+              <li>
+                Wind: <strong>{Math.round(weatherData.wind)} Km/h</strong>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "d4344c6ea95063be2031ed54ff742e0b";
+    let city = "Madrid";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
