0cb12d57ce7ea85016d08c3b53d95120b7ea3f2e
Andreea <>
2021-07-17 14:03:07 +0200

    implemented the icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -5,7 +5,11 @@ export default function WeatherIcon(props) {
     <div className="weather-icon">
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon className="weather-icon" code={props.icon} />
+          <img
+            src={`/images/${props.code}.png`}
+            alt={"weather"}
+            className="weather-icon"
+          />
         </div>
       </div>
     </div>
