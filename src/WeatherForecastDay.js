554b8e9a1b2f1856fd27a33d532161ebb4d9d176
Andreea <>
2021-07-20 19:10:10 +0200

    Styled the UI

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -4,12 +4,12 @@ import WeatherIcon from "./WeatherIcon";
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
     let temperature = Math.round(props.data.temp.max);
-    return `${temperature}º`;
+    return `${temperature}`;
   }
 
   function minTemperature() {
     let temperature = Math.round(props.data.temp.min);
-    return `${temperature}º`;
+    return `${temperature}`;
   }
 
   function day() {
